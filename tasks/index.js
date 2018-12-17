36438cf4973e07d64b4602b7a6c3dde151884922
Fernando Pimenta <fernando.pimenta107@gmail.com>
2018-12-17 19:12:28 -0200

    tasks

---
+++ tasks/index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import Agenda from "./src/screens/Agenda";
+
+AppRegistry.registerComponent(appName, () => Agenda);
