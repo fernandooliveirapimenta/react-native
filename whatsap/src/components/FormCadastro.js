ed49db16ceb671963d8d58972ccaab6a04663037
fernando <fernando.pimenta107@gmail.com>
2018-12-21 00:22:01 -0200

    configurando redux

--- whatsap/src/components/FormCadastro.js
+++ whatsap/src/components/FormCadastro.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import {View, TextInput, Button} from 'react-native';
-
-export default class FormCadastro extends React.Component{
+import {ImageBackground, View, TextInput, Button} from 'react-native';
+import {connect} from 'react-redux';
+import {modificaEmail,modificaSenha,modificaNome} from '../action/AutenticacaoActions';
+class FormCadastro extends React.Component{
     render() {
         return (
-            <View style={{flex: 1, padding: 10}}>
-                <View style={{flex: 4, justifyContent: 'center'}}>
-                    <TextInput placeholder='Nome' style={{fontSize: 20, height: 45}}/>  
-                    <TextInput placeholder='E-mail' style={{fontSize: 20, height: 45}}/>
-                    <TextInput placeholder='Senha' style={{fontSize: 20, height: 45}}/> 
+            <ImageBackground style={{flex: 1, width: null}} source={require('../../assets/bg.png')}>
+                <View style={{flex: 1, padding: 10}}>
+                    <View style={{flex: 4, justifyContent: 'center'}}>
+                        <TextInput value={this.props.nome} placeholder='Nome' style={{fontSize: 20, height: 45}} onChangeText={texto => this.props.modificaNome(texto)}/>  
+                        <TextInput value={this.props.email} placeholder='E-mail' style={{fontSize: 20, height: 45}} onChangeText={texto => this.props.modificaEmail(texto)}/>
+                        <TextInput secureTextEntry value={this.props.senha} placeholder='Senha' style={{fontSize: 20, height: 45}} onChangeText={texto => this.props.modificaSenha(texto)}/> 
+                    </View>
+                    <View style={{flex: 1}}>
+                        <Button title='Cadastrar' color='#115E54' onPress={() => false}/>
+                    </View>
                 </View>
-                <View style={{flex: 1}}>
-                    <Button title='Cadastrar' color='#115E54' onPress={() => false}/>
-                </View>
-            </View>
+            </ImageBackground>
+            
         ); 
     }
 }
+
+const mapStateToProps = state =>  (
+    {
+        nome:state.AutenticacaoReducer.nome,
+        email:state.AutenticacaoReducer.email,
+        senha:state.AutenticacaoReducer.senha,
+    }
+);
+
+export default connect(mapStateToProps, {modificaNome, modificaEmail, modificaSenha})(FormCadastro);
--- whatsap/src/components/FormLogin.js
+++ whatsap/src/components/FormLogin.js
@@ -1,25 +1,45 @@
 import React , {Component} from 'react';
-import {View, Button,Text, TextInput, TouchableHighlight } from 'react-native';
+import {ImageBackground, View, Button,Text, TextInput, TouchableHighlight } from 'react-native';
+import {connect} from 'react-redux';
+import {modificaEmail,modificaSenha} from '../action/AutenticacaoActions';
 
-export default class FormLogin extends Component {
+class FormLogin extends Component {
     render() {
         return(
-            <View style={{flex: 1, padding: 10}}>
-                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
-                    <Text style={{fontSize: 25}}>WhatsApp Clone</Text>
+            <ImageBackground style={{flex:1, width: null}} source={require('../../assets/bg.png')}>
+                <View style={{flex: 1, padding: 10}}>
+                    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+                        <Text style={{fontSize: 25}}>WhatsApp Clone</Text>
+                    </View>
+                    <View style={{flex: 2}}>
+                        <TextInput value={this.props.email}
+                        style={{fontSize: 20, height: 45}}
+                        placeholder='E-mail'
+                        onChangeText={texto => this.props.modificaEmail(texto)}></TextInput>
+                        <TextInput secureTextEntry value={this.props.senha} 
+                        style={{fontSize: 20, height: 45}} 
+                        placeholder='Senha'
+                        onChangeText={texto => this.props.modificaSenha(texto)}></TextInput>
+                        <TouchableHighlight onPress={()=> this.props.navigation.navigate('FormCadastro')}>
+                            <Text style={{fontSize: 20}}>AAinda nao tem cadastro? Cadastre-se</Text>
+                        </TouchableHighlight>
+                    </View>
+                    <View style={{flex: 2}}>
+                        <Button title='Acessar' color='#115E54' onPress={() => false}/>
+                    </View>
                 </View>
-                <View style={{flex: 2}}>
-                    <TextInput style={{fontSize: 20, height: 45}} placeholder='E-mail'>WhatsApp Clone</TextInput>
-                    <TextInput style={{fontSize: 20, height: 45}} placeholder='Senha'>WhatsApp Clone</TextInput>
-                    <TouchableHighlight onPress={()=> this.props.navigation.navigate('FormCadastro')}>
-                        <Text style={{fontSize: 20}}>Ainda naoo tem cadastro? Cadastre-se</Text>
-                    </TouchableHighlight>
-                </View>
-                <View style={{flex: 2}}>
-                    <Button title='Acessar' color='#115E54' onPress={() => false}/>
-                </View>
-            </View>
+            </ImageBackground>
+            
         );
     }
 }
+
+const mapStateToProps = state => (
+    {
+        email: state.AutenticacaoReducer.email,
+        senha: state.AutenticacaoReducer.senha
+    }
+)
+
+export default connect(mapStateToProps, {modificaEmail,modificaSenha})(FormLogin);
     
