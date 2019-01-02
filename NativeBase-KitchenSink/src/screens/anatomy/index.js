f22fc69ba3c76b7f02251c710ce95278bb1d4ff2
Fernando Pimenta <fernando.pimenta107@gmail.com>
2019-01-02 13:15:33 -0200

    nativebase

---
+++ NativeBase-KitchenSink/src/screens/anatomy/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Text,
+  Button,
+  Icon,
+  Footer,
+  FooterTab,
+  Left,
+  Right,
+  Body
+} from "native-base";
+
+import styles from "./styles";
+
+class Anatomy extends Component {
+  render() {
+    return (
+      <Container style={styles.container}>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate("DrawerOpen")}
+            >
+              <Icon name="ios-menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+
+        <Content padder>
+          <Text>Content goes here (internal)</Text>
+        </Content>
+
+        <Footer>
+          <FooterTab>
+              <Text>Footer</Text>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+export default Anatomy;
